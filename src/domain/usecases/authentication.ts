fc70c9ff85a663974b288fb6d69fee4bfae5f407
Junior Martins <amjr.box@gmail.com>
2022-05-03 21:12:14 -0300

    feat: add authentication use case

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,9 @@
+import { AccountModel } from '../models/account-model'
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+export interface IAuthentication {
+  auth(params: AuthenticationParams): Promise<AccountModel>
+}
